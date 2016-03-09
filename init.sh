2c06a9c48ca22cb759a477a63958ba855e84b39f
Sebastien Langoureaux <sebastien.langoureaux@webcenter.fr>
2016-03-09 14:27:58 +0100

    add script to create structure

--- init.sh
+++ init.sh
@@ -3,3 +3,15 @@
 mkdir ember
 mkdir nodejs
 mkdir mongo
+
+echo "Please enter the name of web application (like app-myappUI) :
+read WEB_NAME
+
+echo "Please enter the name of backoffice application (like app-myapp) :
+read BACKOFFICE_NAME
+
+docker-compose run embercli ember new ${WEB_NAME}
+docker-compose run nodejscli yo angular-fullstack ${BACKOFFICE_NAME}
+
+
+docker-compose up 
