54679dcc3b8a8f92c4debfcd5eef0e29b622aa91
Sébastien Langoureaux <sebastien.langoureaux@webcenter.fr>
2016-03-09 23:16:49 +0100

    fix init script

--- init.sh
+++ init.sh
@@ -9,11 +9,13 @@ read APP_WEB
 echo "Please enter the name of backoffice application (like app-myapp) :"
 read APP_BACK
 
-docker-compose run embercli ember new ${APP_WEB}
-docker-compose run nodejscli yo angular-fullstack ${APP_BACK}
+#docker-compose run embercli ember new ${APP_WEB}
+#docker-compose run nodejscli yo angular-fullstack ${APP_BACK}
 
+sed -i 's/.*#APP_WEB/    - .\/workspace\/'"${APP_WEB}"':\/node\/src/g' docker-compose.yml
+sed -i 's/.*#APP_BACK/    - .\/workspace\/'"${APP_BACK}"':\/node\/src/g' docker-compose.yml
 
-sed -i 's/APP_WEB/${APP_WEB}/g' docker-compose.yml
-sed -i 's/APP_BACK/${APP_BACK}/g' docker-compose.yml
+sed -i 's/APP_WEB/'"${APP_WEB}"'/g' docker-compose.yml
+sed -i 's/APP_BACK/'"${APP_BACK}"'/g' docker-compose.yml
 
-docker-compose up 
+#docker-compose up 
